6bf2a06537e41272d44148da71130ef1fbc2a82a
Nedu Robert <nedumrobert@gmail.com>
2018-07-02 19:52:55 +0100

    fixed router error

--- sprintmanager/src/index.js
+++ sprintmanager/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/index.css';
 import App from './components/App';
-import { Router } from 'react-router-dom';
 
 ReactDOM.render(
   <Router>
