f549bb4ceac30fa8f3fd531a04541d4e3779b53a
Nedu Robert <nedumrobert@gmail.com>
2018-07-03 15:02:00 +0100

    Restyled Student component

--- sprintmanager/src/components/Student.js
+++ sprintmanager/src/components/Student.js
@@ -14,11 +14,14 @@ import {
   CardTitle, 
   CardText, 
   Row, 
-  Col
+  Col,
+  Container,
+  Navbar,
+  NavbarBrand
 } from 'reactstrap';
 import classnames from 'classnames';
 
-
+import lambdaLogo from "../img/Lambda_Logo_Full.png";
 import '../css/Student.css';
 
 export default class Student extends Component {
@@ -37,94 +40,98 @@ export default class Student extends Component {
         }
     }
     render() {
-        return <div className="Student-Container">
-            <h3 className="Student-Name">Student Name</h3>
-            <ListGroup>
-                {['Sprint Challenge #1', 'Sprint Challenge #2', 'Sprint Challenge #3'].map(
-                (sprint, index) => {
-                  return (
-                    <React.Fragment>
-                      <ListGroupItem id={ `toggler${index + 1}`}>
-                        {sprint}
-                      </ListGroupItem>
-                          <UncontrolledCollapse toggler={`#toggler${index + 1}`}>
-                        <Nav tabs>
-                          <NavItem>
-                            <NavLink
-                              className={classnames({
-                                active: this.state.activeTab === '1',
-                              })}
-                              onClick={() => {
-                                this.toggle('1');
-                              }}
-                            >
-                            Your Review <Rating
-                                stop={3}
-                                initialRating={index + 1}
-                                readonly
-                            />
-                            </NavLink>
-                          </NavItem>
-                          <NavItem>
-                            <NavLink
-                              className={classnames({
-                                active: this.state.activeTab === '2',
-                              })}
-                              onClick={() => {
-                                this.toggle('2');
-                              }}
-                            >
-                            PM  <Rating
-                                stop={3}
-                                initialRating={index + 1}
-                                readonly
-                            />
-                            </NavLink>
-                          </NavItem>
-                        </Nav>
-                        <TabContent activeTab={this.state.activeTab}>
-                          <TabPane tabId="1">
-                            <Row>
-                              <Col sm="12">
-                                <Card body>
-                                  <CardTitle>
-                                    Student Review Full
-                                  </CardTitle>
-                                  <CardText>
-                                    With supporting text below as a
-                                    natural lead-in to additional
-                                    content.
-                                  </CardText>
-                                  <Button className="Student-Sprint-Link">Go somewhere</Button>
-                                </Card>
-                              </Col>
-                            </Row>
-                          </TabPane>
-                          <TabPane tabId="2">
-                            <Row>
-                              <Col sm="12">
-                                <Card body>
-                                  <CardTitle>
-                                    PM Review Full
-                                  </CardTitle>
-                                  <CardText>
-                                    With supporting text below as a
-                                    natural lead-in to additional
-                                    content.
-                                  </CardText>
-                                  <Button className="Student-Sprint-Link">Go somewhere</Button>
-                                  <Button className="">Go somewhere</Button>
-                                </Card>
-                              </Col>
-                            </Row>
-                          </TabPane>
-                        </TabContent>
-                      </UncontrolledCollapse>
-                    </React.Fragment>
-                  );
-                },
-              )}
+      return <Container className="Student-Container">
+        <Navbar className="NavBar" light expand="md">
+          <NavbarBrand>
+            <img src={lambdaLogo} alt="logo"/>
+          </NavbarBrand>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="#">Welcome Student</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Logout</NavLink>
+            </NavItem>
+          </Nav>
+        </Navbar>
+        <Row className="Student-Name">
+          <Col>
+            <h3>
+              <span>Student Name</span>
+            </h3>
+          </Col>
+        </Row>
+
+        <Row className="Student-Sprint">
+          <Col>
+            <ListGroup className="Student-Sprint--Challenge">
+              {["Sprint Challenge #1", "Sprint Challenge #2", "Sprint Challenge #3"].map((sprint, index) => {
+                return (
+                  <React.Fragment key={`#${index + 1}`}>
+                    <ListGroupItem id={`toggler${index + 1}`}>{sprint}</ListGroupItem>
+                    <UncontrolledCollapse toggler={`#toggler${index + 1}`}>
+                      <Nav className="Student-Sprint-Toggle-Review" tabs>
+                        <NavItem>
+                          <NavLink
+                            className={classnames({
+                              active: this.state.activeTab === "1",
+                            })}
+                            onClick={() => {
+                              this.toggle("1");
+                            }}
+                          >
+                            Your Review <Rating stop={3} initialRating={index + 1} readonly />
+                          </NavLink>
+                        </NavItem>
+                        <NavItem>
+                          <NavLink
+                            className={classnames({
+                              active: this.state.activeTab === "2",
+                            })}
+                            onClick={() => {
+                              this.toggle("2");
+                            }}
+                          >
+                            PM <Rating stop={3} initialRating={index + 1} readonly />
+                          </NavLink>
+                        </NavItem>
+                      </Nav>
+                      <TabContent activeTab={this.state.activeTab}>
+                        <TabPane tabId="1">
+                          <Row>
+                            <Col>
+                              <Card body>
+                                <CardTitle>Student Review Full</CardTitle>
+                                <CardText>
+                                  With supporting text below as a natural lead-in to additional content.
+                                </CardText>
+                                <Button className="Student-Sprint-Link">Github Link to Sprint</Button>
+                              </Card>
+                            </Col>
+                          </Row>
+                        </TabPane>
+                        <TabPane tabId="2">
+                          <Row>
+                            <Col sm="12">
+                              <Card body>
+                                <CardTitle>PM Review Full</CardTitle>
+                                <CardText>
+                                  With supporting text below as a natural lead-in to additional content.
+                                </CardText>
+                                <Button className="Student-Sprint-Link">Edit</Button>
+                                <Button className="Student-Sprint-Link">Submit</Button>
+                              </Card>
+                            </Col>
+                          </Row>
+                        </TabPane>
+                      </TabContent>
+                    </UncontrolledCollapse>
+                  </React.Fragment>
+                );
+              })}
             </ListGroup>
-          </div>;
+          </Col>
+        </Row>
+      </Container>
     }
 }
